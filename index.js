851a86e83e271d7fd49a411d1a362ef2be372ae8
Chuck Burt <chuck.burt+github.com@gmail.com>
2022-11-08 11:45:10 -0500

    modify-for-google-fi-initial

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = Franz => Franz;
+module.exports = Franz => class GoogleFi extends Franz {
+  modifyRequestHeaders() {
+    return [{
+      headers: {
+        origin: 'RefererHost',
+      },
+      requestFilters: {
+        urls: [
+          '*://people-pa.clients6.google.com/*', '*://clients6.google.com/drive/*', '*://clients6.google.com/gmail/*', '*://signaler-pa.clients6.google.com/*', '*://addons-pa.clients6.google.com/*',
+          '*://chat-pa.clients6.google.com/*', '*://clients6.google.com/voice/*', '*://hangouts.google.com/*',
+          '*://peoplestack-pa.clients6.google.com/*', '*://calendar-pa.clients6.google.com/*', '*://play.google.com/*', 
+          '*://jibemessagestore.clients6.google.com/*', '*://instantmessaging-pa-us.clients6.google.com/*'],
+      },
+    }];
+  }
+}
--- webview.js
+++ webview.js
@@ -1,38 +1,3 @@
-/* eslint-disable global-require */
-setTimeout(() => {
-  const elem = document.querySelector('#af-error-container');
-  if (elem && elem.innerText.toLowerCase().includes('the requested url was not found on this server')) {
-    window.location.reload();
-  }
-}, 1000);
-
-window.addEventListener('beforeunload', async () => {
-  try {
-    if (window.FranzAPI) {
-      window.FranzAPI.clearCache();
-    } else {
-      const { remote } = require('electron');
-
-      const webContents = remote.getCurrentWebContents();
-      const { session } = webContents;
-
-      session.flushStorageData();
-      session.clearStorageData({
-        storages: ['appcache', 'serviceworkers', 'cachestorage', 'websql', 'indexdb'],
-      });
-    }
-
-    const registrations = await window.navigator.serviceWorker.getRegistrations();
-
-    registrations.forEach((r) => {
-      r.unregister();
-      console.log('ServiceWorker unregistered');
-    });
-  } catch (err) {
-    console.error(err);
-  }
-});
-
 module.exports = (Franz, settings) => {
   function getMessages() {
     const messages = document.querySelectorAll('.text-content.unread').length;
